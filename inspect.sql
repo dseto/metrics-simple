e1886fd6c4955109bbe627bd0e25a40e19c1b0a2
Daniel Seto <daniel.rubens.seto@gmail.com>
2026-01-05 00:24:13 -0300

    refactor: remove obsolete database inspection scripts and setup files

--- inspect.sql
+++
@@ -1,9 +0,0 @@
--- Check auth_users schema
-.tables
-.schema auth_users
-.schema auth_user_roles
-
--- Check data
-SELECT id, username, display_name, email, substr(password_hash, 1, 20) as password_hash_prefix, is_active FROM auth_users;
-
-SELECT * FROM auth_user_roles;
